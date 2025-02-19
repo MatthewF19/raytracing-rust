57b0356bdd6c04a1197a68e283cab6201a622781
Inferno-idk-what-to-do <matthewfiedler19@gmail.com>
2025-02-19 14:22:54 -0500

    random fuzziness to metal reflections

--- src/material.rs
+++ src/material.rs
@@ -23,11 +23,12 @@ impl Lambertian {
 
 pub struct Metal {
     albedo: Color,
+    fuzz:   f64,
 }
 
 impl Metal {
-    pub fn new(albedo: &Color) -> Self {
-        Self { albedo: *albedo }
+    pub fn new(albedo: &Color, fuzz: f64) -> Self {
+        Self { albedo: *albedo, fuzz: if fuzz < 1.0 { fuzz } else { 1.0 } }
     }
 }
 
@@ -47,10 +48,11 @@ impl Material for Lambertian {
 
 impl Material for Metal {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord, attenuation: &mut Color, scattered: &mut Ray) -> bool {
-        let reflected = Vec3::reflect(r_in.direction(), &rec.normal);
+        let mut reflected = Vec3::reflect(r_in.direction(), &rec.normal);
+        reflected = reflected.unit_vector() + (self.fuzz * Vec3::random_unit_vector());
         
         *scattered = Ray::new(rec.p, reflected);
         *attenuation = self.albedo;
-        return true;
+        return scattered.direction().dot(&rec.normal) > 0.0;
     }
 }
