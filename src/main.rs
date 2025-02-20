1f9d3920ee4f073c2e41576c8f63e2bc0040ecd1
Inferno-idk-what-to-do <matthewfiedler19@gmail.com>
2025-02-20 11:08:12 -0500

    final render for book 1!!!

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@ fn main() -> Result<(), std::io::Error> {
     // world
     let mut world = HittableList::default();
 
+    /*
     let material_ground = Rc::new(Lambertian::new(&Color::new(0.8, 0.8, 0.0)));
     let material_center = Rc::new(Lambertian::new(&Color::new(0.7, 0.4697, 0.7)));
     let material_left = Rc::new(Dielectric::new(1.5));
@@ -38,21 +39,59 @@ fn main() -> Result<(), std::io::Error> {
     world.add(Box::new(Sphere::new(&Vec3::new(-1.0, 0.0, -1.0), 0.5, material_left)));
     world.add(Box::new(Sphere::new(&Vec3::new(-1.0, 0.0, -1.0), 0.4, material_bubble)));
     world.add(Box::new(Sphere::new(&Vec3::new(1.0, 0.0, -1.0), 0.5, material_right)));
+    */
+
+    let ground_mat = Rc::new(Lambertian::new(&Color::new(0.5, 0.5, 0.5)));
+    world.add(Box::new(Sphere::new(&Vec3::new(0.0, -1000.0, 0.0), 1000.0, ground_mat)));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let mat = rand_double();
+            let center = Vec3::new(a as f64 + 0.9*rand_double(), 0.2, b as f64 + 0.9*rand_double());
+            
+            if (center - Vec3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                let sphere_material: Rc<dyn Material>;
+
+                if mat < 0.8 {
+                    let albedo = Color::random() * Color::random();
+                    sphere_material = Rc::new(Lambertian::new(&albedo));
+                    world.add(Box::new(Sphere::new(&center, 0.2, sphere_material)));
+                } else if mat < 0.95 {
+                    let albedo = Color::random_bounded(0.5, 1.0);
+                    let fuzz = rand_range(0.0, 0.5);
+                    sphere_material = Rc::new(Metal::new(&albedo, fuzz));
+                    world.add(Box::new(Sphere::new(&center, 0.2, sphere_material)));
+                } else {
+                    sphere_material = Rc::new(Dielectric::new(1.333));
+                    world.add(Box::new(Sphere::new(&center, 0.2, sphere_material)));
+                }
+            }
+        }
+    }
+
+    let mat1 = Rc::new(Dielectric::new(1.333));
+    world.add(Box::new(Sphere::new(&Vec3::new(0.0, 1.0, 0.0), 1.0, mat1)));
+
+    let mat2 = Rc::new(Lambertian::new(&Color::new(0.7, 0.4697, 0.7)));
+    world.add(Box::new(Sphere::new(&Vec3::new(-4.0, 1.0, 0.0), 1.0, mat2)));
+
+    let mat3 = Rc::new(Metal::new(&Color::new(0.7, 0.6, 0.5), 0.1));
+    world.add(Box::new(Sphere::new(&Vec3::new(4.0, 1.0, 0.0), 1.0, mat3)));
 
     let mut cam = Camera::default();
 
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.img_width = 400;
-    cam.samples_per_pixel = 100;
+    cam.img_width = 1920;
+    cam.samples_per_pixel = 50;
     cam.max_depth = 50;
 
     cam.vfov = 20.0;
-    cam.lookfrom = Vec3::new(-2.0, 2.0, 1.0);
-    cam.lookat = Vec3::new(0.0, 0.0, -1.0);
+    cam.lookfrom = Vec3::new(13.0, 2.0, 3.0);
+    cam.lookat = Vec3::new(0.0, 0.0, 0.0);
     cam.vup = Vec3::new(0.0, 1.0, 0.0);
 
-    cam.defocus_angle = 10.0;
-    cam.focus_dist = 3.4;
+    cam.defocus_angle =0.6;
+    cam.focus_dist = 10.0;
 
     cam.render(&world)?;
 
